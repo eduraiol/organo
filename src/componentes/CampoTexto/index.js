3fa907c8a0f6ea8bdb694338082d8efe357e0461
Eduardo Raiol <marquesraiol@gmail.com>
2023-02-19 23:14:42 -0300

    Criando State de cada input

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,14 +2,9 @@ import { useState } from 'react';
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
-    
-    //let valor = 'Eduardo Raiol';
-
-    const [valor, setValor] = useState('Eduardo Raiol') 
 
     const aoDigitado = (evento) => {
-        setValor(evento.target.value);
-        console.log(valor);
+        props.aoAlterado(evento.target.value)
     }
 
     return (
@@ -17,7 +12,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
